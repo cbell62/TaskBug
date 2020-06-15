98816202e4190fa573d5cef660aa41fd5af73437
Rashaad Ratliff <rratlif3@uncc.edu>
2020-06-15 16:09:53 -0400

    time + manage

---
+++ js/manage.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+function peopleManage() {
+  var time = prompt("Would you like to create a group, add a member to a group, or edit existing members/groups?", "create a group");
+}
---
+++ js/time.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+//function recordTime() {
+//  var time = prompt("Please enter your start time: (hours:minutes)", "00:00");
+//}
+
+
+ function recordTime() {
+      document.querySelector(".overlay").classList.toggle("open");
+ }
